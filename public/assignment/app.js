e900b9263e4fc9469bcf393252c266287b5bbd20
Brendan Ross <brendanros@gmail.com>
2017-02-17 00:06:51 -0500

    Rearranged file structure to avoid triggering myself as much as possible

--- public/assignment/app.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(app) {
-	require("./services/server/user.service.server.js")(app);
-	require("./services/server/website.service.server.js")(app);
-	require("./services/server/page.service.server.js")(app);
-	require("./services/server/widget.service.server.js")(app);
-}
